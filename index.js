9740ac9da7f48059ab3fb6d82b95aa16ac2b4f26
Dat Nguyen <dnguyenk2@gmail.com>
2022-03-26 16:14:47 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(arr, string){
+    
+    return (arr.filter(name => name.toLowerCase() === string.toLowerCase()));
+}
+
+function fuzzyMatch(arr, string){
+
+    return (arr.filter(arr => (arr.includes(string) && arr[0] == string[0])));
+}
+
+function matchName(obj, string){
+
+    return (obj.filter(obj => obj.name === string));
+}
